273b55fd12c969e16463d75f4b74a18ed12ac2df
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2018-08-28 19:57:48 +0800

    latest edition!

--- _20_CafeTask.py
+++ _20_CafeTask.py
@@ -1,3 +1,5 @@
+from _19_InterpreterForCSV import *
+
 linecounter = 1  # sets variable linecounter to 1
 logr = input('LOG (True/False): ')  # sets boolean logr to to user assigned bool (for logging purposes)
 print()
@@ -24,30 +26,57 @@ def error(err, string):
     return err
 
 
+def create(gn, sn, storage):
+    def nv(string):
+        return str(input(string))
+
+    data = Read_csv(storage, True)
+    if data[0].startswith('!ERROR') or not ', '.join(data[0]) == 'GNAME, SNAME, ROLE, TRATE, SUPER, HLTH, MON, TUE, WED, THU, FRI, SAT, SUN, LASTCHECKIN, LASTCHECKOUT':
+        write_csv_record(storage, [
+            ['GNAME', 'SNAME', 'ROLE', 'TRATE', 'SUPER', 'HLTH', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN',
+             'LASTCHECKIN', 'LASTCHECKOUT']])
+    print('Please enter the information for ' + gn + ' ' + sn + '.')
+    role = nv('ROLE (Barista/Manager): ').strip()
+    role = role.lower()
+    if role == 'barista' or role == 'manager':
+        pass
+    else:
+        error('!ERROR! Incorrect ROLE input.')
+    trate = nv('TRATE (30/40)').strip()
+    if trate == '30' or trate == '40':
+        pass
+    else:
+        error('!ERROR! Incorrect TRATE input.')
+    trate = nv('TRATE (4/6/8)').strip()
+    if trate == '4' or trate == '6' or 8 :
+        pass
+    else:
+        error('!ERROR! Incorrect TRATE input.')
+    append_csv_record(storage, [gn, sn, role, trate, superr, hlth, mon, tue, wed, thu, fri, sat, sun, lastcheckin, lastcheckout])
+
+
 def run():
-    print('Welcome to Cafe')
+    storage = 'cafedata.csv'
+    print('!NOTE! This program stores information in ' + storage + '.\nWelcome to Cafe')
     print('Please choose your action: ')
     print('     1 - Check In')
     print('     2 - Check Out')
     print('     3 - Calculate Wages')
-    print('     4 - Update Employee Data')
-    print('     5 - Create Employee Data')
+    print('     4 - Create Employee Data')
     try:
         x = int(input('  -> '))
-    except:
+    except ValueError:
         error('!ERROR_1!', 'Incorrect Input')
     gname = str(input('\nEmployee GNAME: '))
     sname = str(input('\nEmployee SNAME: '))
     if x == 1:
-        print(checkin(gname, sname))
+        print(checkin(gname, sname, storage))
     elif x == 2:
-        print(checkout(gname, sname))
+        print(checkout(gname, sname, storage))
     elif x == 3:
-        print(wages(gname, sname))
+        print(wages(gname, sname, storage))
     elif x == 4:
-        print(update(gname, sname))
-    elif x == 5:
-        print(create(gname, sname))
+        print(create(gname, sname, storage))
     else:
         error('!ERROR_2!', 'Incorrect Input')
 
