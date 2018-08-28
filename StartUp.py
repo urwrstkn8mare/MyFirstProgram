9962bdc4102a7237e92e18879e596eb1ff335482
urwrstkn8mare <urwrstkn8mare@users.noreply.github.com>
2018-08-28 19:59:54 +0800

    removed duplicate info from README.md from StartUp.py

--- StartUp.py
+++ StartUp.py
@@ -19,17 +19,3 @@ while True:
         print('')
     else:
         break
-
-# INFO
-"""
-Each of the files in the projects folder is run from here.
-The numbers on each file correspond to the number assigned to them in the importing function.
-To understand more just look at the code.
-:)
-The tester file is for testing and has no use. There is just some random stuff on there i use that
-probably doesnt work.
-Each version (verson number in this file) is the the next time I look at this project with all it's new stuff
-and clean it up (or give it the cherry on top). Then I give it the new version number.
-lr (last revision) is the date this file was last worked on.
-If you are not Samit then the date is when you were given this project to see.
-"""
