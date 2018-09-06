475212c563172305910c5aacdf25e2424621ee33
urwrstkn8mare <urwrstkn8mare@users.noreply.github.com>
2018-09-06 19:18:44 +0800

    Still not completely finished but almost done. Gotta finish commenting. Still runnable now. :)

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # MyFirstProgram
 
-**NOTE *The latest version of _19_InterpreterForCSV.pyis accidentally broken (I committed early). Please use the latest commit before the one that says unfinished.***
-
 ## About
 This is where almost all my work on python both in and out of school is stored. Nobody but me works on it unless otherwise decided. This is just for my own education or experience and serves no actual use.
 
 ## Info
-Each of the files in the projects folder is run from here. The numbers on each file correspond to the number assigned to them in the importing function. To understand more just look at the code. The tester file is for testing and has no use. There is just some random stuff on there I use that probably doesnt work. :)
+Each of the files in the projects folder is run from here. The numbers on each file correspond to the number assigned to them in the importing function. To understand more just look at the code. The tester file is for testing and has no use. There is just some random stuff on there I use that probably doesn't work. :)
--- _19_InterpreterForCSV.py
+++ _19_InterpreterForCSV.py
@@ -3,127 +3,130 @@ logr = input('LOG (True/False): ').lower() in ['true', '1', 't', 'y', 'yes', 'ye
 print()  # Print new line
 
 
-def log(name, text, validation):  # Define function log with parameters: name, text and validation
-    if validation:  # If variable validation is True then...
-        print(
-            'LOG>>> ' + str(text) + ' <<< ' + str(name))  # ... print 'LOG>>>', variable text, '<<<' then variable name.
-
+def log(text, validator, **opt):  # Define function log with parameters: name, text and validator
+    name = '' # Store an empty string in variable name.
+    if 'name' in opt: # If there is an optional parameter named 'name' then...
+        name = opt['name'] # ...store 'name' of the optional parameters in name.
+    if validator:  # If variable validator is True then...
+        print('LOG>>> ' + str(text) + ' <<< ' + str(name))  # ... print 'LOG>>>', variable text, '<<<' then variable nameself.
+    if 'wait' in opt: # If there is an optional parameter named 'wait' then...
+        if opt['wait']: # ...if 'wait' of opt is True then...
+            input('LOG>>> Enter to continue >>>') # ...wait for user to enter to continue.
 
 def error(err, string):  # Define function error with parameters: err and string
     print(err + ' ' + string)  # Print variables err and string with a space in between.
     return err  # Return variable err.
 
 
-def exist(filename):
-    try:
-        open(str(filename))  # tries to open file
-    except FileNotFoundError:
-        return False  # if error then returns false
-    return True  # else returns true
+def exist(filename): # Define function exist with parameter filename/
+    try: # Tries...
+        opener = open(str(filename))  # ...to open file which name is stored in variable filename.
+    except FileNotFoundError: # If there is an error, FileNotFoundError, then
+        return False  # ...return False.
+    return True  # If there is no error, return True.
 
 
-def read_csv(filename, header):
-    while True:  # infinite loop unless breaked
-        if filename.endswith('.csv'):  # if filename ends with '.csv'
+def read_csv(filename, header): # Define function read_csv with parameters: filename and header.
+    while True:  # Infinitely loop the following until broken.
+        if filename.endswith('.csv'): # If variable filename's string ends with '.csv' then...
             try:
-                with open(str(filename)) as fr:  # tries to read filename
-                    content = fr.readlines()
-                    content = [xx.strip() for xx in content]  # strips whitespace
-            except FileNotFoundError:  # if file not found then outputs custom error
+                with open(str(filename)) as fr: # ...try to open file which name is stored in variable filename as fr.
+                    content = fr.readlines() # Store list containing each line in the opened file, fr, as a list item.
+                    content = [xx.strip() for xx in content] # Removes whitespace (eg. ' ', '\n') from each line in list variable content.
+            except FileNotFoundError: # If there is an error, FileNotFoundError, then...
                 return error('!ERROR_02!',
                              'The file you gave was not found or there was a problem with the program. (the file must '
-                             'be in the same directory as this program.)')  #
-            for i in range(len(content)):
-                if content[i].find(',') == -1:
-                    return error('!ERROR_03!', 'Not formatted correctly according to CSV format.')
-            log('VERIFIED')
-            pp = []
-            log('LOOP X')
-            for x in range(len(content)):
-                xx = content[x].split(',')
-                b = []
-                log('LOOP C')
-                for c in range(len(xx)):
-                    log('LOOP Y')
-                    for y in range(len(xx)):
-                        z = ','.join(xx[c:y + 1])
-                        z = z.strip()
-                        log('zandmore -> ' + z + str(z.startswith('\"')) + str(z.endswith('\"')))
-                        if z.startswith('\"') and z.endswith('\"'):
-                            log('[c, y + 1] -> ' + str([c, y + 1]))
-                            b.append([c, y + 1])
-                b.append([-1, -1])
-                log('b -> ' + str(b))
-                p = []
-                m = 0
-                u = 0
-                log('len(xx) -> ' + str(len(xx)))
-                log('LOOP U')
-                while u < len(xx):
-                    if u == b[m][0]:
-                        o = ','.join(xx[u:b[m][1]])
-                        log('join -> ' + ','.join(xx[u:b[m][1]]))
-                        u = b[m][1]
-                        log('u -> ' + str(u))
-                    else:
-                        o = xx[u]
-                        log('xx[u] -> ' + xx[u])
+                             'be in the same directory as this program.)')  # return a custom error.
+            for i in range(len(content)): # While variable i is in the range of the number of list items in content...
+                if not ',' in content[i]: # If there is not a ',' in variable i of content...
+                    return error('!ERROR_03!', 'Not formatted correctly according to CSV format.') # ..return a custom error.
+            log('VERIFIED', logr) # Log 'VERIFIED' if logr is True.
+            pp = [] # Store an empty list in variable pp.
+            log('LOOP x', logr) # Log 'LOOP X' if logr is true.
+            for x in range(len(content)): # While variable x is in the range of the number of list items in content...
+                xx = content[x].split(',') # ...split x of content around the commas and store the list output in variable xx.
+                b = [] # Store an empty list in variable b.
+                log('LOOP c', logr) # Log 'LOOP C' if logr is True.
+                for c in range(len(xx)): # While variable c is in the range of the number of items in list variable xx...
+                    log('LOOP y', logr) # ...log 'LOOP Y' if logr is True.
+                    for y in range(len(xx)): # While variable y is in the range of the number of items in the list variable xx...
+                        z = ','.join(xx[c:y + 1]) # ...join variable c to variable y plus 1 of list variable xx with commas and store it in variable z.
+                        z = z.strip() # Remove whitespace (eg. ' ', '\n') from variable z.
+                        log(z, logr, name='z')  # Log variable z if logr is True and with the name: 'z'.
+                        log(str(z.startswith('\"')), logr, name='z.startswith(\'\"\')') # Log True if z starts with '"' if logr is true and with the name: 'z.startswith('"')'.
+                        log(str(z.endswith('\"')), logr, name='z.endswith(\'\"\')') # Log True if z ends with '"' if logr is true and with the name: 'z.endswith('"')'.
+                        if z.startswith('\"') and z.endswith('\"'): # If z starts with and ends with '""' then...
+                            log(str([c, y + 1]), logr, name='[c, y + 1]') # ...log list containing c and y plus 1 if logr is True and with the name: '[c, y + 1]'.
+                            b.append([c, y + 1]) # Append list containing c and y plus 1 to list variable b.
+                b.append([-1, -1]) # Append list containing negative 1 and negative 1 to list variabel b.
+                log(str(b), logr, name='b') # Log variable b if logr is True with the name: 'b'.
+                p = [] # Store empty list in variable p.
+                m = 0 # Store zero in variable m.
+                u = 0 # Store zero in variable u.
+                log(len(xx), logr, name='len(xx)') # Log the number of items in xx if logr is True with the name: 'len(xx)'.
+                log('LOOP U', logr) # Log 'LOOP U' if logr is True.
+                while u < len(xx): # While u is under the number of items in xx...
+                    if u == b[m][0]: # ...,if u is the same as 0 of m of b then...
+                        o = ','.join(xx[u:b[m][1]]) # ...join u to 1 of m of b of with commas and then store it in variable o.
+                        log(o, logr, name='o (True)') # Log variable o if logr is True with the name: 'o (True)'.
+                        u = b[m][1] # Store 1 of m of b in variable u.
+                        log(u, logr, name='u (True)') # Log variable u if logr is True with the name: 'u (True)'.
+                    else: # Else...
+                        o = xx[u] # ...store u of xx in o.
+                        log(o, logr, name='o (False)') # Log variable o if logr is True with the name: 'o (False)'.
                         u += 1
-                    p.append(o.strip())
-                log('p -> ' + str(p))
+                        log(u, logr, name='u (False)') # Log variable u if logr is True with the name: 'u (False)'.
+                    p.append(o.strip()) # Append o with whitespace removed to p.
+                log(p, logr, name='pp')
                 pp.append(p)
-                log('pp -> ' + str(pp))
+                log(pp, logr, name='pp')
             old = len(pp[0])
-            log('LOOP H')
+            log('LOOP H', logr)
             for h in range(len(pp)):
                 new = len(pp[h])
-                log('old -> ' + str(old))
-                log('new -> ' + str(new))
+                log(old, logr, name='old')
+                log(new, logr, name='new')
                 if old == new:
                     old = new
                 else:
                     return error('!ERROR_04!', 'Not formatted correctly according to CSV format.')
-            log('pp -> ' + str(pp))
+            log(pp, logr, name='pp')
         else:
             return error('!ERROR_01!', 'The file does not have a .csv extension.')
         keys = []
         csvfile = []
-        log('LOOP G')
+        log('LOOP G', logr)
         for g in range(len(pp[0])):
             if header:
                 keys.append(pp[0][g])
             else:
                 keys.append(g)
-        log('keys -> ' + str(keys))
-        log('header -> ' + str(header))
+        log(keys, logr, name='keys')
+        log(header, logr, name='header')
         if header:
             q = 1
         else:
             q = 0
-        log('LOOP Q')
+        log('LOOP Q', logr)
         while q < len(pp):
             b = {}
             for j in range(len(pp[q])):
                 b[keys[j]] = pp[q][j]
             csvfile.append(b)
             q += 1
-        log('csvfile -> ' + str(csvfile))
+        log(csvfile, logr, name='csvfile')
         return csvfile
 
 
 def find_csv_record(filename, key_value, **key):
     # there must be header and header must have a key field to use
     csvfile = read_csv(filename, True)
-    log('csvfile -> ' + str(csvfile))
     if 'key' in key:
         keyy = key['key']
     else:
         keyy = 'key'
     for l in range(len(csvfile)):
-        log(l)
         try:
-            log('csvfile[l][keyy] -> ' + str(csvfile[l][keyy]))
-            log('key_value -> ' + str(key_value))
             if csvfile[l][keyy] == key_value:
                 return csvfile[l]
         except IndexError:
@@ -134,13 +137,10 @@ def find_csv_record(filename, key_value, **key):
 
 def update_csv_record(filename, header, row, field, new_value):
     csvfile = read_csv(filename, header)
-    log('csvfile -> ' + str(csvfile))
     try:
         csvfile[row][field] = new_value
     except IndexError:
         return error('!ERROR! Row and field are not found in file.')
-    log('csvfile -> ' + str(csvfile))
-    log('csvfile[row][field] -> ' + str(csvfile))
     newfile = [', '.join(csvfile[0])]
     for l in range(len(csvfile)):
         ll = ','.join(csvfile[l])
@@ -149,12 +149,10 @@ def update_csv_record(filename, header, row, field, new_value):
         for g in range(len(ll)):
             pp.append(csvfile[l][ll[g]])
         newfile.append(', '.join(pp))
-    log('newfile -> ' + str(newfile))
     f = open(filename, "w")
     writeto = ''
     for h in range(len(newfile)):
         writeto = writeto + newfile[h] + '\n'
-    log('writeto -> ' + str(writeto))
     f.write(str(writeto))
     f.close()
 
