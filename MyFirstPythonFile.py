136c84a8ea8e39b9c82d027f10c1d25ee66f7093
urwrstkn8mare <shaikhsamit43@gmail.com>
2018-09-20 19:25:37 +0800

    CS 20/9/2018

--- MyFirstPythonFile.py
+++ MyFirstPythonFile.py
@@ -14,9 +14,9 @@ def prg(prgnumber):
     path = os.path.abspath(os.path.dirname(sys.argv[0])) + '/'
     find = '_' + "{0:0=2d}".format(prgnumber) + '_'
     modulepath = [i for i in os.listdir(path) if os.path.isfile(os.path.join(path, i)) and find in i]
-    if modulepath[0] == '':
+    if not modulepath:
         print('Could not find project.\nEnding...')
-        exit()
+        return 'NOTFOUND'
     else:
         module = modulepath[0].split('.')
         imported = __import__(module[0])
@@ -24,36 +24,89 @@ def prg(prgnumber):
     return modulepath[0]
 
 
-# cmdline
+# cmd
 def cmd():
-    def cmdprint():
-        print(input('PRINT: '))
+    def cmdprint(order):
+        order = str(order[0])
+        print(str(order))
 
-    command = str(input('\nCMD: ')).strip()
+    def newproject(title):
+        title = str(title[0])
+        template = '# Created ' + str(
+            datetime.datetime.now()) + '\n# ' + title + '\n\ndef run():\n   # Mainline code goes below' \
+                                                        ', any functions, imports or global variables ' \
+                                                        'can go outside as long as\n   # this is the ' \
+                                                        'code that is meant to run.\n\n# Ignore below ' \
+                                                        'this line.\nif __name__ == \'__main__\':\n   ' \
+                                                        'run()'
+        z = 0
+        while True:
+            path = os.path.abspath(os.path.dirname(sys.argv[0])) + '/'
+            find = '_' + "{0:0=2d}".format(z) + '_'
+            modulepath = [l for l in os.listdir(path) if os.path.isfile(os.path.join(path, l)) and find in l]
+            if not modulepath:
+                break
+            else:
+                z += 1
+        with open('_' + "{0:0=2d}".format(z) + '_' + title + '.py', 'w+') as f:
+            f.write(template)
+        f.close()
+
+    def prgs():
+        z = 0
+        files = []
+        while True:
+            path = os.path.abspath(os.path.dirname(sys.argv[0])) + '/'
+            find = '_' + "{0:0=2d}".format(z) + '_'
+            modulepath = [l for l in os.listdir(path) if os.path.isfile(os.path.join(path, l)) and find in l]
+            if not modulepath:
+                break
+            else:
+                files.append(modulepath[0])
+                z += 1
+        print('MyFirstProgram')
+        for h in range(len(files)):
+            print(' -->' + str(files[h]))
+
+    command = str(input('\n   cmd: ')).strip()
     print('-----------------\n\n')
     cmds = {
         'print': cmdprint,
-        'time': lambda: print(datetime.datetime.now())
+        'time': lambda dud: print(datetime.datetime.now()),
+        'prg': lambda number: prg(int(number[0])),
+        'newprg': newproject,
+        'prgs': lambda dud: prgs()
     }
     end = 0
     start = 0
     nowtime = 0
-    for i in range(len(cmds)):
-        ran = True
-        try:
-            start = time.time()
-            nowtime = datetime.datetime.now()
-            cmds[command]()
-            end = time.time()
-        except KeyError:
-            ran = False
-        if ran:
-            break
-        print('\n\n-----------------')
+    ran = False
+    if '-' in command:
+        command = command.strip().split('-', 1)
+        commands = [str(command[0]), command[1].strip().split(',')]
+        for i in range(len(cmds)):
+            ran = True
+            try:
+                start = time.time()
+                nowtime = datetime.datetime.now()
+                try:
+                    cmds[commands[0]](commands[1])
+                except IndexError:
+                    print('\n\n-----------------\nNot enough parameters')
+                    return
+                end = time.time()
+            except KeyError:
+                ran = False
+            if ran:
+                break
+    else:
+        print('\n\n-----------------\nNo seperating dash found.')
+        return
+    print('\n\n-----------------')
     if not ran:
         print('Command Not Found...')
     else:
-        print('[' + command + ']' + ' Finished Running.')
+        print('[' + commands[0] + ']' + ' Finished Running.')
         print('Runtime: ' + str(end - start) + 's - elapsed time')
         print('         ' + str(time.process_time()) + 's - processing time')
         print('Run at ' + str(nowtime) + '\n')
